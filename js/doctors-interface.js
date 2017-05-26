6841b08d82e94c9517a71bc124c96f2badfffaa5
JonathonKang <jonkang@hotmail.com>
2017-05-26 11:14:41 -0700

    Define back end and front end functions for querying API based on user search parameters.

--- js/doctors-interface.js
+++ js/doctors-interface.js
@@ -4,23 +4,24 @@ var getDoctors = require('./../js/doctors.js').getDoctors;
 //query, first, last, location/range, specialty, key
 
 var displayDoctors = function(result){
-  result.forEach(function(doctor){
+  result.data.forEach(function(doctor){
     //append a div
     console.log(doctor);
   });
-}
+};
 
 $(document).ready(function(){
 
   $('form').submit(function(e){
     e.preventDefault();
     queryParams = {
-      query: $('#condition').val(),
-      first: $('#firstName').val(),
-      last: $('#lastName').val(),
+      condition: $('#condition').val(),
+      firstName: $('#firstName').val(),
+      lastName: $('#lastName').val(),
       specialty: $('#specialties').val(),
       key: apiKey,
-    }
+      range: $('#range').val(),
+    };
     getDoctors(queryParams, displayDoctors);
-  })
+  });
 });
--- js/doctors.js
+++ js/doctors.js
@@ -11,12 +11,9 @@
 // }
 
 exports.getDoctors = function(queryParams, displayDoctors) {
-  // https://api.betterdoctor.com/2016-03-01/doctors?first_name=test&last_name=test&query=test&specialty_uid=test&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=7fc91d9851939ecec74cb6aaff0866dc
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
+  navigator.geolocation.getCurrentPosition(function(position){
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?first_name='+queryParams.firstName+'&last_name='+queryParams.lastName+'&query='+queryParams.condition+'&specialty_uid='+queryParams.specialty+'&location='+position.coords.latitude+'%2C'+position.coords.longitude+'%2C'+queryParams.range+'&user_location='+position.coords.latitude+'%2C'+position.coords.longitude+'&skip=0&limit=10&user_key=' + queryParams.key, function(result){
       displayDoctors(result);
     })
-   .fail(function(error){
-      console.log("fail");
-    });
+  });
 };
